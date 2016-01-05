572df0ef32b0de3e644edc1b2857820a5d105d78
Szigeti Gábor <szigeti@niif.hu>
2016-01-05 13:42:08 +0100

    better structure of code

--- Floor_heating_control/EEPROM_control.ino
+++ Floor_heating_control/EEPROM_control.ino
@@ -9,6 +9,7 @@ void initEEPROM() {
   if ( reset ) {
     EEPROM.put( eeAddress, false );
     Serial.println("reset set to false");
+    TempSensorData* currentSensor;
     for (int i=0; i<rooms; i++) {
     
       digitalWrite(ledPin, HIGH);   // sets the LED on
@@ -17,30 +18,25 @@ void initEEPROM() {
       eeAddress = sizeof(reset)+i*sizeof(TempSensorData); //Move address to the next byte after float 'f'.
       TempSensorData SensorData; //Variable to store custom object read from EEPROM.
     
-      if ( i == 0 ) {
-        memcpy ( SensorData.addr, sensor0.addr, sizeof(sensor0.addr)+1 );   //SensorData.addr = sensor0.addr;
-        SensorData.alarm = sensor0.alarm;
-        SensorData.delta = sensor0.delta;
-        strcpy (SensorData.name,sensor0.name);
+      switch (i) {
+        case 0:
+          currentSensor = &sensor0;
+          break;
+        case 1:
+          currentSensor = &sensor1;
+          break;
+        case 2:
+          currentSensor = &sensor2;
+          break;
+        case 3:
+          currentSensor = &sensor3;
+          break;
       }
-      if ( i == 1 ) {
-        memcpy ( SensorData.addr, sensor1.addr, sizeof(sensor1.addr)+1 );   //SensorData.addr = sensor1.addr;
-        SensorData.alarm = sensor1.alarm;
-        SensorData.delta = sensor1.delta;
-        strcpy (SensorData.name,sensor1.name);
-      }
-      if ( i == 2 ) {
-        memcpy ( SensorData.addr, sensor2.addr, sizeof(sensor2.addr)+1 );   //SensorData.addr = sensor2.addr;
-        SensorData.alarm = sensor2.alarm;
-        SensorData.delta = sensor2.delta;
-        strcpy (SensorData.name,sensor2.name);
-      }
-      if ( i == 3 ) {
-        memcpy ( SensorData.addr, sensor3.addr, sizeof(sensor3.addr)+1 );   //SensorData.addr = sensor3.addr;
-        SensorData.alarm = sensor3.alarm;
-        SensorData.delta = sensor3.delta;
-        strcpy (SensorData.name,sensor3.name);
-      }
+
+      memcpy ( SensorData.addr, currentSensor->addr, sizeof(currentSensor->addr)+1 );   //SensorData.addr = sensor3.addr;
+      SensorData.alarm = currentSensor->alarm;
+      SensorData.delta = currentSensor->delta;
+      strcpy (SensorData.name,currentSensor->name);
      
 
       Serial.print(SensorData.addr[0]);
