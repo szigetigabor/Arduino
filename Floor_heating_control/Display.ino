d88b3b069e4fc7aae45d5ff8b858889f570e9523
Szigeti Gábor <szigeti@niif.hu>
2016-01-19 16:00:18 +0100

    add UTFT display control

---
+++ Floor_heating_control/Display.ino
@@ -0,0 +1,65 @@
+#include "Display_includes.h"
+
+
+int current_page;
+int i; //TODO remove
+
+
+
+//UTFT          myGLCD(HX8357B,38,39,40,41);
+//UTFT          myGLCD(QDM320B,38,39,40,41);
+UTFT          myGLCD(CTE32HR,38,39,40,41);
+
+
+UTouch        myTouch(6,5,4,3,2);
+
+// Finally we set up UTFT_Buttons :)
+UTFT_Buttons  myButtons(&myGLCD, &myTouch);
+
+
+void initDisplay(){
+  myGLCD.InitLCD();//PORTRAIT);
+  myGLCD.clrScr();
+  myGLCD.setFont(hungarian_font_16x16);
+  myGLCD.setBackColor(DEFAULT_BACKGROUND);
+  //myGLCD.setBackColor(VGA_WHITE);
+ // myGLCD.setBackColor(VGA_RED);
+  //myGLCD.setBackColor(VGA_GREEN);
+  //myGLCD.setBackColor(VGA_BLUE);
+  //myGLCD.setBackColor(VGA_SILVER);
+  //myGLCD.setBackColor(VGA_GRAY);
+  //myGLCD.setBackColor(VGA_MAROON);
+  //myGLCD.setBackColor(VGA_YELLOW);
+  //myGLCD.setBackColor(VGA_OLIVE);
+  //myGLCD.setBackColor(VGA_LIME);
+  //myGLCD.setBackColor(VGA_AQUA);
+  //myGLCD.setBackColor(VGA_TEAL);
+  //myGLCD.setBackColor(VGA_NAVY);
+  //myGLCD.setBackColor(VGA_FUCHSIA);
+  //myGLCD.setBackColor(VGA_PURPLE);
+  //myGLCD.setBackColor(VGA_TRANSPARENT);
+
+  //myGLCD.fillScr(255,255,255);
+  myGLCD.fillScr(VGA_BLACK);
+
+  myGLCD.setBrightness(1);
+  myGLCD.setContrast(1);
+
+  //myTouch.InitTouch(PORTRAIT);
+  //myTouch.setPrecision(PREC_MEDIUM);
+  
+  myButtons.setTextFont(hungarian_font_16x16);
+  myButtons.setSymbolFont(Dingbats1_XL);
+
+  current_page = 1;
+}
+
+void showGUI(){
+  if ( current_page == 1 ) {
+    showAlarmSettingGUI(i);
+  }
+  delay(2000);
+  i = (i+1) % 4;
+  Serial.println(i);
+}
+
---
+++ Floor_heating_control/Display_includes.h
@@ -0,0 +1,47 @@
+#ifndef DISPLAY_INCLUDES
+#define DISPLAY_INCLUDES
+
+#include <UTFT.h>
+#include <memorysaver.h>
+#include <UTouch.h>
+#include <UTFT_Buttons.h>
+
+word DEFAULT_BACKGROUND = VGA_BLACK;
+word DEFAULT_FONT_COLOR = VGA_WHITE;
+
+#include <UTFT_Buttons.h>
+extern UTFT          myGLCD;
+extern UTFT_Buttons  myButtons;
+extern UTouch        myTouch;
+
+// Declare which fonts we will be using
+extern uint8_t SmallFont[];
+extern uint8_t BigFont[];
+extern uint8_t Dingbats1_XL[];
+extern uint8_t franklingothic_normal[];
+//extern uint8_t SevenSegNumFontPlusPlus[];
+extern uint8_t GroteskBold32x64[];
+extern uint8_t hungarian_font_16x16[];
+
+void showTitle( char title[20], int offset ) {
+  myGLCD.setColor(VGA_BLUE);
+  myGLCD.fillRect(0, 0, 480, 35);
+  myGLCD.setColor(VGA_WHITE);
+  myGLCD.setBackColor(VGA_BLUE);
+  myGLCD.print(title, 240-offset, 10);
+
+  //set default color
+  myGLCD.setColor(DEFAULT_FONT_COLOR);
+  myGLCD.setBackColor(DEFAULT_BACKGROUND);
+
+}
+
+int showBackButton() {
+//back button
+  int but_back = myButtons.addButton( 1, 1, 50,  50, "9", BUTTON_SYMBOL);
+  myButtons.drawButton(but_back);
+  return but_back;  
+}
+
+
+#endif
