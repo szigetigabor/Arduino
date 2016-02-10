162ff1b2e6044d89488e633cc5e2f9703a7ca695
Szigeti Gábor <szigeti@niif.hu>
2016-02-10 15:55:30 +0100

    touch update

--- Floor_heating_control/settingsGUI.h
+++ Floor_heating_control/settingsGUI.h
@@ -21,27 +21,34 @@ void showSettingsButtons() {
   myButtons.drawButton(but_disp);
 
 }
-
+void initSettingsGUI() {
+  prev_page = PAGE_SETTINGS;
+  touched = false;
+  myButtons.deleteAllButtons();
+  myGLCD.clrScr();
+}
 void showSettingsGUI() {
-  //myButtons.deleteAllButtons();
-  showTitle("Be{ll\"t{sok");
-  showSettingsButtons();
-  but_back = showBackButton();
+  if ( current_page != prev_page || touched) {
+    initSettingsGUI();
+    showTitle("Be{ll\"t{sok");
+    showSettingsButtons();
+    but_back = showBackButton();
+  }
 
   if (myTouch.dataAvailable() == true) {
     pressed_button = myButtons.checkButtons();
     if (pressed_button==but_back) {
-      current_page = 1;
+      current_page = PAGE_MAIN;
     } else if (pressed_button==but_date) {
-      current_page = 4;
+      current_page = PAGE_DATE;
     } else if (pressed_button==but_alarm) {
-      current_page = 5;
+      current_page = PAGE_REQ_TEMP;
     } else if (pressed_button==but_night) {
-      current_page = 6;
+      current_page = PAGE_REQ_NIGHT_TEMP;
     } else if (pressed_button==but_sched) {
-      current_page = 7;
+      current_page = PAGE_MODE_TIMING;
     } else if (pressed_button==but_disp) {
-      current_page = 8;
+      current_page = PAGE_DISPLAY;
     }
   }
 }
