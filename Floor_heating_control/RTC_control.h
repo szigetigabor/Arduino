8612a4884300b7d45590d4ff222e2ef5da662527
Szigeti Gábor <szigeti@niif.hu>
2016-02-25 10:48:08 +0100

    update time only one per minute

--- Floor_heating_control/RTC_control.h
+++ Floor_heating_control/RTC_control.h
@@ -30,27 +30,29 @@ void readRTC() {
   prevRTC = tmpRTC;
   tmElements_t tm;
   if (RTC.read(tm)) {
-    Serial.print("Ok, Time = ");
-    print2digits(tm.Hour);
-    Serial.write(':');
-    print2digits(tm.Minute);
-    Serial.write(':');
-    print2digits(tm.Second);
-    Serial.print(", Date (D/M/Y) = ");
-    Serial.print(tm.Day);
-    Serial.write('/');
-    Serial.print(Mon[tm.Month]);
-    Serial.write('/');
-    Serial.print(tmYearToCalendar(tm.Year));
-    Serial.write(' ');
-    Serial.print(Day[tm.Wday]);
-    Serial.println();
-    tmpRTC.tHour   = tm.Hour;
-    tmpRTC.tMinute = tm.Minute;
-    tmpRTC.tDow    = tm.Wday;
-    tmpRTC.tDay    = tm.Day;
-    tmpRTC.tMonth  = tm.Month;
-    tmpRTC.tYear   = tmYearToCalendar(tm.Year);
+    if ( tmpRTC.tMinute != tm.Minute ) {
+      Serial.print("Ok, Time = ");
+      print2digits(tm.Hour);
+      Serial.write(':');
+      print2digits(tm.Minute);
+      Serial.write(':');
+      print2digits(tm.Second);
+      Serial.print(", Date (D/M/Y) = ");
+      Serial.print(tm.Day);
+      Serial.write('/');
+      Serial.print(Mon[tm.Month]);
+      Serial.write('/');
+      Serial.print(tmYearToCalendar(tm.Year));
+      Serial.write(' ');
+      Serial.print(Day[tm.Wday]);
+      Serial.println();
+      tmpRTC.tHour   = tm.Hour;
+      tmpRTC.tMinute = tm.Minute;
+      tmpRTC.tDow    = tm.Wday;
+      tmpRTC.tDay    = tm.Day;
+      tmpRTC.tMonth  = tm.Month;
+      tmpRTC.tYear   = tmYearToCalendar(tm.Year);
+    }
   } else {
     if (RTC.chipPresent()) {
       Serial.println("The DS1307 is stopped.  Please run the SetTime");
