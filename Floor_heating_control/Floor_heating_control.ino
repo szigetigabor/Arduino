a6c61c84410fe16edd2f1ed2b7c643ceab445a5e
Szigeti Gábor <szigeti@niif.hu>
2016-12-20 22:15:41 +0100

    boiler checking by the temp reading

--- Floor_heating_control/Floor_heating_control.ino
+++ Floor_heating_control/Floor_heating_control.ino
@@ -53,7 +53,6 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   readRTC();
-  boilerIsRunning = boilerRunningCheck();
   readTemperatures();
   readExtraTemperatures();
   showGUI();
