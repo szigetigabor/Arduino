836d56960b3d149801dc49d571e4f731ac90bf0d
Szigeti Gábor <szigeti@niif.hu>
2016-12-25 16:32:36 +0100

    update mapping info

--- Floor_heating_control/EEPROM_control.h
+++ Floor_heating_control/EEPROM_control.h
@@ -11,8 +11,10 @@
  *  0. reset
  *  1. blacklight
  *  2. sleep time
- *  3-x. room infos (39 bytes each info)
- *  x-y. extra temperature sensors
+ *  3-x. room infos (41 bytes each info)
+ *  x-y. extra temperature sensors (28 bytes each info)
+ *  y-499. empty space
+ *  500-z. scheduling
  */
 #define BASE_OFFSET 3
 
