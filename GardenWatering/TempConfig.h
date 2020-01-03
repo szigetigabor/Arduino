9cd7f9ac337ad1a73bb2696267397ffcaf6d1432
szigetigabor <szigeti@elte.hu>
2020-01-03 18:02:00 +0100

    1-wire temperature management

---
+++ GardenWatering/TempConfig.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef TEMP_CONFIG_H
+#define TEMP_CONFIG_H
+
+#define NR_OF_SENSORS 2
+#define ONEWIRE_PIN 2
+
+static byte addr1[8]={0x28, 0x35, 0x49, 0x79, 0x97, 0x10, 0x03, 0x65};
+static byte addr2[8]={0x28, 0xD8, 0x68, 0x79, 0x97, 0x10, 0x03, 0x33};
+
+static byte* sensorAddresses[NR_OF_SENSORS] = {addr1, addr2};
+#endif
